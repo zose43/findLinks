8422abeaf60a89f54e94a809da5eff6b2bf6349b
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-04-11 17:31:49 +0300

    upd

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"os"
+	"strings"
 )
+
+var selectors = map[string]int{}
 
 func main() {
 	doc, err := html.Parse(os.Stdin)
@@ -15,6 +18,10 @@ func main() {
 	for _, link := range visit(nil, doc) {
 		println(link)
 	}
+	fmt.Printf("Count different selectors in doc=%d\n", len(selectors))
+	for t, c := range selectors {
+		fmt.Printf("%s %d\n", t, c)
+	}
 }
 
 func visit(links []string, n *html.Node) []string {
@@ -23,6 +30,15 @@ func visit(links []string, n *html.Node) []string {
 			if a.Key == "href" {
 				links = append(links, a.Val)
 			}
+		}
+	}
+	if n.Type == html.ElementNode {
+		selectors[n.Data]++
+	}
+	if n.Parent != nil {
+		t := strings.TrimSpace(n.Data)
+		if n.Parent.Data != "script" && n.Parent.Data != "style" && n.Type == html.TextNode && t != "" {
+			fmt.Printf("%s\n", n.Data)
 		}
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
