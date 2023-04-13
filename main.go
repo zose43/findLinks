2e45b38d88cedb5a5aba2747da93313e02b1e6b7
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-04-13 13:36:36 +0300

    add outline

--- main.go
+++ main.go
@@ -1,86 +1,62 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"golang.org/x/net/html"
 	"net/http"
 	"os"
-	"regexp"
-	"strings"
 )
 
-var selectors = map[string]int{}
-
-type Counter struct {
-	Images, Words int
-}
+var depth int
 
 func main() {
-	c := new(Counter)
-	for _, url := range os.Args[1:] {
-		links, err := findLinks(url, c)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "FindLinks %s\t%v", url, err)
-			continue
-		}
-		for _, link := range links {
-			println(link)
-		}
-		fmt.Printf("Count different selectors in doc=%d\n", len(selectors))
-		for t, c := range selectors {
-			fmt.Printf("%s %d\n", t, c)
-		}
-		fmt.Printf("Words count: %d\tImages count: %d\n", c.Words, c.Images)
+	url, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	err := findLinks(url[:len(url)-1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "FindLinks %s\t%v", url, err)
 	}
 }
 
-func findLinks(url string, c *Counter) ([]string, error) {
+func findLinks(url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Resp code %d\t%s\t%s", resp.StatusCode, url, resp.Status)
+		return fmt.Errorf("Resp code %d\t%s\t%s", resp.StatusCode, url, resp.Status)
 	}
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Invalid HTML parse %s\t%v", url, err)
+		return fmt.Errorf("Invalid HTML parse %s\t%v", url, err)
 	}
-	return visit(nil, c, doc), nil
+	forEachNode(doc, startElement, endElement)
+	return nil
 }
 
-func visit(links []string, count *Counter, n *html.Node) []string {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				links = append(links, a.Val)
-			}
-		}
-	}
-	if n.Type == html.ElementNode {
-		selectors[n.Data]++
-	}
-	if n.Parent != nil {
-		t := strings.TrimSpace(n.Data)
-		if n.Parent.Data != "script" && n.Parent.Data != "style" && n.Type == html.TextNode && t != "" {
-			fmt.Printf("%s\n", n.Data)
-		}
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = visit(links, count, c)
-		countWordsAndImages(c, count)
-
+		forEachNode(c, startElement, endElement)
 	}
-	return links
+	if post != nil {
+		post(n)
+	}
 }
 
-func countWordsAndImages(n *html.Node, c *Counter) {
-	if n.Type == html.ElementNode && n.Data == "img" {
-		c.Images++
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
 	}
-	if n.Type == html.TextNode {
-		t := strings.TrimSpace(n.Data)
-		c.Words += len(regexp.MustCompile(`\ss`).Split(t, -1))
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
 	}
 }
