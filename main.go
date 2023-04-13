c1bc93ea675be69b6621faedaf8eec2036bb758e
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-04-14 00:50:38 +0300

    done

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 	"strings"
 )
 
-var depth int
+var attrID string
+var stopIter = false
 
 func main() {
 	url, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	attrID, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+	attrID = strings.TrimSpace(attrID)
 	err := findLinks(url[:len(url)-1])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "FindLinks %s\t%v", url, err)
@@ -36,50 +39,45 @@ func findLinks(url string) error {
 	return nil
 }
 
-func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+func forEachNode(n *html.Node, pre, post func(n *html.Node) bool) {
 	if pre != nil {
-		pre(n)
+		if pre(n) {
+			stopIter = true
+		}
 	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
+	for c := n.FirstChild; c != nil && stopIter; c = c.NextSibling {
 		forEachNode(c, startElement, endElement)
 	}
 	if post != nil {
-		post(n)
-	}
-}
-
-func startElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		if closeTag(n) {
-			fmt.Printf("%*s<%s %s/>\n", depth*2, "", n.Data, printAttrs(n.Attr))
-		} else {
-			fmt.Printf("%*s<%s %s>\n", depth*2, "", n.Data, printAttrs(n.Attr))
-			depth++
+		if post(n) {
+			stopIter = true
 		}
 	}
-	if n.Type == html.TextNode && strings.TrimSpace(n.Data) != "" {
-		fmt.Printf("%*s%s\n", depth*2, "", n.Data)
-	}
 }
 
-func endElement(n *html.Node) {
-	if n.Type == html.ElementNode && !closeTag(n) {
-		depth--
-		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
-	}
+func startElement(n *html.Node) bool {
+	return checkNode(n)
 }
 
-func printAttrs(attrs []html.Attribute) string {
-	attr := ""
-	for _, item := range attrs {
-		attr += fmt.Sprintf("%s=\"%s\" ", item.Key, item.Val)
-	}
-	return attr
+func endElement(n *html.Node) bool {
+	return checkNode(n)
 }
 
-func closeTag(n *html.Node) bool {
-	if n.FirstChild == nil && n.NextSibling != nil && strings.TrimSpace(n.NextSibling.Data) == "" {
+func checkNode(n *html.Node) bool {
+	if findByID(n, attrID) != nil {
+		fmt.Printf("%s is find\n", attrID)
 		return true
 	}
 	return false
+}
+
+func findByID(n *html.Node, id string) *html.Node {
+	if n.Type == html.ElementNode {
+		for _, item := range n.Attr {
+			if item.Key == "id" && item.Val == id {
+				return n
+			}
+		}
+	}
+	return nil
 }
